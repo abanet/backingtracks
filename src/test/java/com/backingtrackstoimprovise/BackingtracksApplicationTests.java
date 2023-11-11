f34d1067f4c15a244b97aacfb528cc3c13ebdf28
Alberto Banet <alberto.banet@gmail.com>
2023-11-11 17:59:21 +0100

    Primer commit

---
+++ src/test/java/com/backingtrackstoimprovise/BackingtracksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backingtrackstoimprovise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackingtracksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
